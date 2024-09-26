6db631c7e7371c6e607ddced96096480037a454c
Esther Mutua <155438090+Esther-pixel31@users.noreply.github.com>
2024-09-27 01:00:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const searchButton = document.querySelector('.search-bar button');
+function updateDateTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
+    document.getElementById('date-time').textContent = `Today, ${timeString}`;
+}
+updateDateTime();
+setInterval(updateDateTime, 60000); // Update every minute
+
+const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-form-input');
 const cityElement = document.getElementById('city');
 
 searchButton.addEventListener('click', handleSearchSubmit);
 
-function handleSearchSubmit() {
-  cityElement.textContent = searchInput.value;
-  searchInput.value = ''; // clear the input field
-}
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    cityElement.textContent = searchInput.value;
+    searchInput.value = ''; 
+    }
+
