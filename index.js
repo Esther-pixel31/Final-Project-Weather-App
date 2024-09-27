edcd65b75f0a8f45349e37e633e06c2106f4e0d7
Esther Mutua <155438090+Esther-pixel31@users.noreply.github.com>
2024-09-27 14:20:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,43 @@ const searchInput = document.getElementById('search-form-input');
 
 searchButton.addEventListener('click', handleSearchSubmit);
 
+function getIconClass(condition) {
+  let iconClasses = {
+    'Sunny': 'icon-sunny',
+    'Partly Cloudy': 'icon-partly-cloudy',
+    'Cloudy': 'icon-cloudy',
+    'Overcast': 'icon-overcast',
+    'Light Rain': 'icon-light-rain',
+    'Heavy Rain': 'icon-heavy-rain',
+    'Thunderstorm': 'icon-thunderstorm',
+    'Light Snow': 'icon-light-snow',
+    'Heavy Snow': 'icon-heavy-snow',
+    'Light Sleet': 'icon-light-sleet',
+    'Heavy Sleet': 'icon-heavy-sleet',
+    'Light Freezing Rain': 'icon-light-freezing-rain',
+    'Heavy Freezing Rain': 'icon-heavy-freezing-rain',
+    'Light Drizzle': 'icon-light-drizzle',
+    'Heavy Drizzle': 'icon-heavy-drizzle',
+    'Light Freezing Drizzle': 'icon-light-freezing-drizzle',
+    'Heavy Freezing Drizzle': 'icon-heavy-freezing-drizzle',
+    'Light Rain Shower': 'icon-light-rain-shower',
+    'Heavy Rain Shower': 'icon-heavy-rain-shower',
+    'Light Sleet Shower': 'icon-light-sleet-shower',
+    'Heavy Sleet Shower': 'icon-heavy-sleet-shower',
+    'Light Hail': 'icon-light-hail',
+    'Heavy Hail': 'icon-heavy-hail',
+    'Light Hail Shower': 'icon-light-hail-shower',
+    'Heavy Hail Shower': 'icon-heavy-hail-shower',
+    'Light Thunderstorm': 'icon-light-thunderstorm',
+    'Heavy Thunderstorm': 'icon-heavy-thunderstorm',
+    'Light Fog': 'icon-light-fog',
+    'Heavy Fog': 'icon-heavy-fog',
+    'Light Freezing Fog': 'icon-light-freezing-fog',
+    'Heavy Freezing Fog': 'icon-heavy-freezing-fog'
+  };
+  return iconClasses[condition];
+}
+
 function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.current.temp_c;
@@ -26,25 +63,55 @@ function refreshWeather(response){
     cityElement.innerHTML = response.data.location.name;
     
     let descriptionElement = document.querySelector("#description");
-    let weatherCode = response.data.current.condition.code;
-
-    let weatherDescriptions = {
-    '1000': 'Sunny',
-    '1003': 'Partly Cloudy',
-    '1006': 'Cloudy',
-    '1009': 'Overcast',
-    '1063': 'Light Rain',
-    '1069': 'Heavy Rain',
-    '1072': 'Thunderstorm',
-    // Add more weather codes as needed
-  };
-
-  let description = weatherDescriptions[weatherCode];
 
     temperatureElement.innerHTML= `${Math.round(temperature)}°`;
     humidityElement.innerHTML = `<i class="fas fa-tint"></i> ${humidity}%`;
     windElement.innerHTML = `<i class="fas fa-wind"></i> ${wind.toFixed(0)}km/h`;
     descriptionElement.innerHTML = description;
+
+
+    let uvIndex = response.data.current.uv;
+  let pollutionIndex = response.data.current.pollution;
+  let pollenIndex = response.data.current.pollen;
+
+  let uvIndicator = document.querySelector(".high");
+  let pollutionIndicator = document.querySelector(".low");
+  let pollenIndicator = document.querySelector(".moderate");
+
+  if (uvIndex < 3) {
+    uvIndicator.textContent = "Low";
+    uvIndicator.style.color = "green";
+  } else if (uvIndex < 6) {
+    uvIndicator.textContent = "Moderate";
+    uvIndicator.style.color = "orange";
+  } else {
+    uvIndicator.textContent = "High";
+    uvIndicator.style.color = "red";
+  }
+
+  if (pollutionIndex < 50) {
+    pollutionIndicator.textContent = "Low";
+    pollutionIndicator.style.color = "green";
+  } else if (pollutionIndex < 100) {
+    pollutionIndicator.textContent = "Moderate";
+    pollutionIndicator.style.color = "orange";
+  } else {
+    pollutionIndicator.textContent = "High";
+    pollutionIndicator.style.color = "red";
+  }
+
+  if (pollenIndex < 50) {
+    pollenIndicator.textContent = "Low";
+    pollenIndicator.style.color = "green";
+  } else if (pollenIndex < 100) {
+    pollenIndicator.textContent = "Moderate";
+    pollenIndicator.style.color = "orange";
+  } else {
+    pollenIndicator.textContent = "High";
+    pollenIndicator.style.color = "red";
+  }
+
+
 }
 
 function handleSearchSubmit(event) {
@@ -56,5 +123,7 @@ function handleSearchSubmit(event) {
     
   searchInput.value = '';
 }
+
+
 
 
